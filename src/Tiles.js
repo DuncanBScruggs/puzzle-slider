5d2d1419bb8800b9fe6e10ca7b8b2ef04615cb3b
DuncanBScruggs <duncanbscruggs@gmail.com>
2020-10-28 14:35:45 -0400

    refactored the rows and columns

--- src/Tiles.js
+++ src/Tiles.js
@@ -25,19 +25,28 @@ function Tiles(props) {
     let grid = props.data.map((y, j) => {
         return (
             (j + 1) % 4 === 0 &&
-            <div className="row">
+            <div className="row d-flex justify-content-center">
                 {props.data.slice(j + 1 - 4, j + 1).map((item, index) => (
-                    <div id={index} name={item.id} className="col-3 border border-dark " onClick={() => props.moveTile(index)}>
+                    <div id={index} name={item.id} className="col-3 border border-dark " onClick={() => props.moveTile(index)}
+                        style={{
+                            maxWidth: "75px",
+                            minWidth: "75px",
+                            maxHeight: "75px",
+                            minHeight: "75px",
+                            overflow: "hidden",
+                            margin: "0",
+                            padding: "0"
+                        }}
+                    >
                         {item.id}
                     </div>
                 ))}
             </div>
         )
     })
-    console.log(grid)
 
     return (
-        <div className="container">
+        <div className="container my-5">
             {grid}
         </div>
     )
